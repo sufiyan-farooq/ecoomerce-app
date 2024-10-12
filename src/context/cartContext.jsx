05a98eb9371bd4aba5e7e8495718daa13963bd0b
sualeh-farooq <138825396+sufiyan-farooq@users.noreply.github.com>
2024-10-12 17:15:45 +0500

    build fixes

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,34 +1,36 @@
 import { createContext } from "react";
 import { useState } from "react";
-export const CartContext = createContext()
+export const CartContext = createContext();
 
-export default function CartContextProvider({children} ){
-    const [cartItems, setCartItems] = useState([])
+export default function CartContextProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
 
-    const addItemToCart = (item) => {
+  const addItemToCart = (item) => {
+    const arr = cartItems;
+    const findIndex = cartItems.findIndex((data) => data.id === item.id);
 
-        const arr = cartItems
-        const findIndex = cartItems.findIndex((data)=>data.id === item.id )
-
-        if (findIndex === -1) {
-            arr.push(item)
-        } else {
-            arr[findIndex].quantity++
-        }
-        setCartItems([...arr])
+    if (findIndex === -1) {
+      arr.push(item);
+    } else {
+      arr[findIndex].quantity++;
     }
+    setCartItems([...arr]);
+  };
 
-    const totalPrice = cartItems.reduce(
-        (total, item) => total + item.price * item.quantity,
-        0
-      );
-    return (
-        <CartContext.Provider value={{
-            cartItems ,
-             addItemToCart ,
-              setCartItems,
-              totalPrice}} > 
-{children}
-        </CartContext.Provider>
-    )
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addItemToCart,
+        setCartItems,
+        totalPrice,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
